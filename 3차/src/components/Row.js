8629a9cb3b06d0e6e1376703cbacb0ebfb3e5a59
cheej10 <cheej10@naver.com>
2022-03-21 15:03:28 +0900

    add footer and modal

---
+++ 3차/src/components/Row.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../api/axios';
+import './Row.css';
+import MovieModal from './MovieModal/index';
+
+export default function Row({ isLargeRow, title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+  };
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div
+          className="slider_arrow-left"
+          onClick={() => {
+            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+          }}
+        >
+          <span className="arrow">{'<'}</span>
+        </div>
+        <div id={id} className="row_posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              } `}
+              alt={movie.name}
+              onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+        <div
+          className="slider_arrow-right"
+          onClick={() => {
+            document.getElementById(id).scrollLeft += window.innerWidth - 80;
+          }}
+        >
+          <span className="arrow">{'>'}</span>
+        </div>
+      </div>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen}></MovieModal>
+      )}
+    </section>
+  );
+}
